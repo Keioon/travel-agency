7197b82263ef7494927ee24fc1decedb9d3e7ff0
Keioon <78851196+Keioon@users.noreply.github.com>
2021-05-10 21:06:05 +0200

    TripSummary Corection 3

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -38,7 +38,8 @@ describe('Component TripSummary', () => {
     expect(component.find('.tags span').at(2).text()).toEqual(expectedTags[2]);
   });
   it('should not render div tags if props tags is false', () => {
-    const component = shallow(<TripSummary id={'/trip/abc'} image={'image.jpg'} name={'alt'} cost={'1'} days={1} tags={['tag1', 'tag2', 'tag3']} />);
-    expect(component.hasClass('tags')).toBe(false);
+    const component = shallow(<TripSummary id={'/trip/abc'} image={'image.jpg'} name={'alt'} cost={'1'} days={1} tags={[]} />);
+    console.log(component.debug());
+    expect(component.find('.tags span').exists()).toBe(false);
   });
 });
