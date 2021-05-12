c2e0604eabf8d63b66477d69f389d1ec1c584a28
Keioon <78851196+Keioon@users.noreply.github.com>
2021-05-12 11:20:20 +0200

    Adding sending order on Appi

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,24 +4,59 @@ import OrderSummary from '../OrderSummary/OrderSummary';
 import PropTypes from 'prop-types';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
 
-const OrderForm = ({options, setOrderOption, tripCost}) => (
+const sendOrder = (options, tripCost, tripDetails) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+    ...tripDetails,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
+
+const OrderForm = props => (
   <Row>
     {pricing.map(option =>
       <Col md={4} key={option.id}>
-        <OrderOption {...option} currentValue = {options[option.id]} setOrderOption = {setOrderOption}/>
+        <OrderOption {...option} currentValue = {props.options[option.id]} setOrderOption = {props.setOrderOption}/>
       </Col>
     )}
     <Col xs={12}>
-      <OrderSummary cost={tripCost} options={options}/>
+      <OrderSummary cost={props.tripCost} options={props.options}/>
     </Col>
+    <Button onClick={() => sendOrder(props.options, props.tripCost, props.tripDetails)}>Order now!</Button>
   </Row>
 );
 
 OrderForm.propTypes = {
-  options: PropTypes.object,
+  options: PropTypes.any,
   tripCost: PropTypes.string,
   setOrderOption: PropTypes.func,
+  name: PropTypes.string,
+  tripDetails: PropTypes.object,
 };
 
 export default OrderForm;
